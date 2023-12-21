c54d18cd998f36b52a85402935c9c0ebbec9c9b0
HarshalRajendraPatil <102664407+HarshalRajendraPatil@users.noreply.github.com>
2023-12-22 00:32:11 +0530

    added animation to entier website

--- main.js
+++ main.js
@@ -1,3 +1,89 @@
+// Variables for showing the animations
+const sections = document.querySelectorAll("section");
+const homeSec = document.querySelector(".homePage");
+const aboutSec = document.querySelector(".about");
+const weddingSec = document.querySelector(".wedding");
+const gallerySec = document.querySelector(".gallery");
+const groom = document.querySelector(".groom");
+const bride = document.querySelector(".bride");
+const gName = document.querySelector(".gName");
+const bName = document.querySelector(".bName");
+const emoji = document.querySelector(".emoji");
+const timeLeft = document.querySelector(".timeLeft");
+const mhendi = document.querySelector(".mhendi");
+const haldi = document.querySelector(".haldi");
+const marriage = document.querySelector(".marriage");
+const map = document.querySelector(".location");
+const box = document.querySelector(".box");
+const dream = document.querySelectorAll(".dream");
+
+if (homeSec.classList.contains("show-animate")) {
+  gName.classList.add("animate-gName");
+  bName.classList.add("animate-bName");
+  emoji.classList.add("animate-emoji");
+  timeLeft.classList.add("animate-timeLeft");
+} else {
+  timeLeft.classList.remove("animate-timeLeft");
+  gName.classList.remove("animate-gName");
+  bName.classList.remove("animate-bName");
+  emoji.classList.remove("animate-emoji");
+}
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+
+    if (top >= offset && top < offset + height) {
+      sec.classList.add("show-animate");
+    } else {
+      sec.classList.remove("show-animate");
+    }
+
+    if (homeSec.classList.contains("show-animate")) {
+      gName.classList.add("animate-gName");
+      bName.classList.add("animate-bName");
+      emoji.classList.add("animate-emoji");
+      timeLeft.classList.add("animate-timeLeft");
+    } else {
+      timeLeft.classList.remove("animate-timeLeft");
+      gName.classList.remove("animate-gName");
+      bName.classList.remove("animate-bName");
+      emoji.classList.remove("animate-emoji");
+    }
+
+    if (aboutSec.classList.contains("show-animate")) {
+      groom.classList.add("animate-groom");
+      bride.classList.add("animate-bride");
+    } else {
+      groom.classList.remove("animate-groom");
+      bride.classList.remove("animate-bride");
+    }
+
+    if (weddingSec.classList.contains("show-animate")) {
+      mhendi.classList.add("animate-gName");
+      haldi.classList.add("animate-emoji");
+      marriage.classList.add("animate-bName");
+      map.classList.add("animate-map");
+    } else {
+      map.classList.remove("animate-map");
+      mhendi.classList.remove("animate-gName");
+      haldi.classList.remove("animate-emoji");
+      marriage.classList.remove("animate-bName");
+    }
+
+    if (gallerySec.classList.contains("show-animate")) {
+      box.classList.add("animate-box");
+      dream.forEach((d) => d.classList.add("animate-dream"));
+    } else {
+      box.classList.remove("animate-box");
+      dream.forEach((d) => d.classList.remove("animate-dream"));
+    }
+  });
+};
+
+// Variables for showing time-left
 const day = document.querySelectorAll(".day");
 const hour = document.querySelectorAll(".hour");
 const minute = document.querySelectorAll(".minute");
